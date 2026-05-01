154e10053e010a860d88df0fb69c36200b753847
Laila Ahmed <laila.ahmed.2645@gmail.com>
2026-05-01 14:16:29 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
+# 1. No arguments, prints greeting
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+# 2. One argument, prints greeting
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
+# 3. One argument with default value
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
+# 4. Returns sum of two numbers
 def add(num1, num2):
-    pass
+    return num1 + num2
 
+# 5. Returns half of a number
 def halve(number):
-    pass
+    return number / 2
